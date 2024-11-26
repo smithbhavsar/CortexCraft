c5f76cea7e0d70d476339b45b8ef3593670ec074
Smith bhavsar <smithbhavsar1328@gmail.com>
2024-11-27 01:50:56 +0530

    Initial commit

--- cortexcraft-backend/app/core/config.py
+++ cortexcraft-backend/app/core/config.py
@@ -1,15 +1,13 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    # Database configuration
-    DATABASE_URL: str = "postgresql://postgres:Vartika123@db-cortexcraft.cjua400sqzt6.us-east-1.rds.amazonaws.com:5432/db-cortexcraft"
-
-    # AWS Configuration
-    AWS_ACCESS_KEY_ID: str
-    AWS_SECRET_ACCESS_KEY: str
-    S3_BUCKET_NAME: str = "cortex-craft-assets"
+    app_name: str = "CortexCraft"
+    aws_access_key_id: str
+    aws_secret_access_key: str
+    s3_bucket_name: str
+    database_url: str
 
     class Config:
-        env_file = ".env"
+         env_file = "../.env"
 
 settings = Settings()
