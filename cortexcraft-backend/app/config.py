0d261756bb34b920c7b9e3efc9b99d9a5079e07d
Smith bhavsar <smithbhavsar1328@gmail.com>
2024-12-02 13:07:25 +0530

    Add local postgres steup

--- cortexcraft-backend/app/config.py
+++ cortexcraft-backend/app/config.py
@@ -1,12 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://username:password@rds-endpoint:5432/cortexcraft_db"
-    AWS_ACCESS_KEY_ID: str
-    AWS_SECRET_ACCESS_KEY: str
-    AWS_REGION: str = "us-east-1"
-
-    class Config:
-        env_file = ".env"
+    DATABASE_URL: str = "postgresql://postgres:password@localhost:5432/cortexcraft_db"
 
 settings = Settings()
+
